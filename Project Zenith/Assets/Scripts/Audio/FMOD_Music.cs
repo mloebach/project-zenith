01047a35a43d98dc50a52dee839f93b79ff2c254
mloebach <50314703+mloebach@users.noreply.github.com>
2021-06-18 19:39:58 -0700

    initial push

---
+++ Project Zenith/Assets/Scripts/Audio/FMOD_Music.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMOD_Music : MonoBehaviour
+{
+    FMOD.Studio.EventInstance BGM;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BGM = FMODUnity.RuntimeManager.CreateInstance("event:/Main BGM");
+    }
+
+    public void PlayBGM(){
+        BGM.start();
+    }
+
+    public void PlaySong(string song){
+        BGM.setParameterByName(song, 1f);
+        //BGM.setParameterValue(song, 1f);
+    }
+
+    public void StopBGM(){
+        BGM.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    private void OnDestroy(){
+        BGM.release();
+    }
+}
